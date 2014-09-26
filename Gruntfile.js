ec3b6d87ea481e9ce2a178684149e08c8db9a246
Felix Barros <felix.ricardo.barros@gmail.com>
2014-09-25 23:42:15 -0300

    Configuramos grunt

---
+++ Gruntfile.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = function (grunt) {
+	
+	/** Almazenar las tarea en la carpeta 'grunt'.
+	 * @link http://firstandthird.github.io/load-grunt-config/
+	 */
+	require('load-grunt-config')(grunt);
+
+};
